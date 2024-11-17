d71313cb91d8ba0cee55940f0c91dac09c28a09b
marcos-erbas-jr <marcospaulosejr360@gmail.com>
2024-11-16 21:14:35 -0300

    Movimentação da bolinha adicionada

---
+++ start.py
@@ -0,0 +1,38 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((535, 330))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+x = (screen.get_width()/2)-8
+y = (screen.get_height()/2)-5
+a = 10
+b = 10
+
+def 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.draw.lines(screen, (255, 255, 255), True,[(10, 10), (525, 10), (10, 10), (10, 320), (525, 320),(525, 10)], 5)
+    pygame.draw.line(screen, (255, 255, 255), (263, 10), (263, 320), 5)
+
+
+    pygame.draw.rect(screen, (255, 255, 255), (x, y, 10, 10))
+
+    x += a
+    y += b/4
+    if x >= 510 or x <=10:
+        a *= -1
+
+    if y >= 315 or y <= 10:
+        b *= -1
+
+
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
